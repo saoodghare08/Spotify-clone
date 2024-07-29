695e37f9c7289998478b854cdf8ffb2f86efd060
saoodghare08 <saoodghare0809@gmail.com>
2024-07-29 20:39:42 +0530

    first commit

---
+++ src/components/Songitem.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
+
+const Songitem = ({name, image, desc, id}) => {
+
+  const {playWithId} = useContext(PlayerContext)
+  return (
+    <div onClick={()=>playWithId(id)} className='min-2-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+        <img src={image} className='rounded' alt="" />
+        <p className='font-bold mt-2 mb-1'>{name}</p>
+        <p className=' text-slate-200 text-sm'></p>
+      
+    </div>
+  )
+}
+
+export default Songitem
